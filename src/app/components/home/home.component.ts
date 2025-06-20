cdf017ef8c9436e9e5260c44400196b8edf53ea7
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-06-19 21:35:46 -0300

    improved ux

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TabelaComponent } from "../tabela/tabela.component";
 import { InputsComponent } from "../inputs/inputs.component";
+import { NavHeaderComponent } from "../nav-header/nav-header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TabelaComponent, InputsComponent],
+  imports: [TabelaComponent, InputsComponent, NavHeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/components/nav-header/nav-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-header',
+  imports: [],
+  templateUrl: './nav-header.component.html',
+  styleUrl: './nav-header.component.css'
+})
+export class NavHeaderComponent {
+
+}
